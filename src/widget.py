4463765ff9ab1ca38b24ea71eb74d4ba60318b70
Svetlana Bazhenova <varta.73@mail.ru>
2024-07-22 14:07:19 +0300

    Сохранение функции даты

---
+++ src/widget.py
@@ -0,0 +1,15 @@
+from src\masks import get_mask_card_number, get_mask_account
+from datetime import datetime
+
+def mask_account_card():
+
+
+def get_data(data: str) -> str:
+    """Функция, принимающая на вход данные о дате и времени и возвращает строку с датой в формате
+"ДД.ММ.ГГГГ"""
+    dat = datetime.strptime(data, format("%Y-%m-%dT%H:%M:%S.%f"))
+    return (dat.strftime("%d.%m.%Y"))
+
+print (get_data("2024-07-22T02:26:18.671407"))
+
+
