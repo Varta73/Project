fa7f932c81ffe7e396fd94dffa5ac7ca22e842c3
Svetlana Bazhenova <varta.73@mail.ru>
2024-07-22 23:06:34 +0300

    Добавление функции маскировки карт и счетов

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,16 @@
 from src.masks import get_mask_account
+from src.masks import get_mask_card_number
 from datetime import datetime
 
-def mask_account_card():
-
+def mask_account_card(string: str) -> str:
+    """Функция маскировки номера карты или счета"""
+    if "Счет" in string:
+        acc_number = get_mask_account(string[:])
+        return acc_number
+    else:
+        card_number = get_mask_card_number(string[:])
+        return card_number
+# print(mask_account_card("Счет 73654108430135874305"))
 
 def get_data(data: str) -> str:
     """Функция принимает на вход данные о дате и времени и возвращает строку с датой в формате
@@ -10,6 +18,6 @@ def get_data(data: str) -> str:
     dat = datetime.strptime(data, format("%Y-%m-%dT%H:%M:%S.%f"))
     return (dat.strftime("%d.%m.%Y"))
 
-print (get_data("2024-07-22T02:26:18.671407"))
+# print (get_data("2024-07-22T02:26:18.671407"))
 
 
