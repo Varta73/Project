b59fc89b81b21f073042ac0eb6622ddfa605b910
Svetlana Bazhenova <varta.73@mail.ru>
2024-07-22 23:46:06 +0300

    Окончательный вариант

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,14 @@
-def get_mask_card_number(card_number: str) -> str | None:
+def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера банковской карты"""
     if len(card_number) == 24 or len(card_number) > 25:
-        return f"{card_number[:-17]} {card_number[-16:-12]} {card_number[-12:-10]}{'*' * 2} {'*' * 4} {card_number[-4:]}"
+        return (
+            f"{card_number[:-17]} {card_number[-16:-12]} {card_number[-12:-10]}{'*' * 2} {'*' * 4} {card_number[-4:]}"
+        )
     else:
         return None
 
 
-def get_mask_account(acc_number: str) -> str | None:
+def get_mask_account(acc_number: str) -> str:
     """Функция маскировки номера банковского счёта"""
     if "Счет":
         return f"Счет {'*' * 2}{acc_number[-4::]}"
--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,7 @@
-from src.masks import get_mask_account
-from src.masks import get_mask_card_number
 from datetime import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(string: str) -> str:
     """Функция маскировки номера карты или счета"""
@@ -10,14 +11,16 @@ def mask_account_card(string: str) -> str:
     else:
         card_number = get_mask_card_number(string[:])
         return card_number
+
+
 # print(mask_account_card("Счет 73654108430135874305"))
+
 
 def get_data(data: str) -> str:
     """Функция принимает на вход данные о дате и времени и возвращает строку с датой в формате
-"ДД.ММ.ГГГГ"""
+    "ДД.ММ.ГГГГ"""
     dat = datetime.strptime(data, format("%Y-%m-%dT%H:%M:%S.%f"))
-    return (dat.strftime("%d.%m.%Y"))
-
-# print (get_data("2024-07-22T02:26:18.671407"))
+    return dat.strftime("%d.%m.%Y")
 
 
+# print(get_data("2024-07-22T02:26:18.671407"))
