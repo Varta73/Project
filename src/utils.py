b91879eb2d4280d5547a809b6810096bddd073a1
Svetlana Bazhenova <varta.73@mail.ru>
2024-08-20 16:28:09 +0300

    Dобавление логгера для модуля utils

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,22 @@ import json
 from typing import Any
 import json
 from typing import Any
+import logging
+import os
+
+dir1 = os.path.dirname(os.path.abspath(__file__))
+path_1 = os.path.join(dir1, "../src/utils.log")
+path_2 = os.path.abspath(path_1)
+
+# Логгер, который записывает логи в файл.
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(path_2, "w", encoding="utf-8")
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 def get_transaction(file_json: str) -> Any:
     """
